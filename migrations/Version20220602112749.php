d79c666a377d2f163de079cf8fd9e1ba3158ded2
JeremyGlize <JeremGlize@gmail.com>
2022-06-02 13:28:33 +0200

    add and delete migration

--- migrations/Version20220602112749.php
+++ migrations/Version20220602112749.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220516205820 extends AbstractMigration
+final class Version20220602112749 extends AbstractMigration
 {
     public function getDescription(): string
     {
